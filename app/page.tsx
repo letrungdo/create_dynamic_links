dc01e17f0d1aea1f80d0c9f6f0fa0c3be6c74c31
DoLT <dolt@guide.inc>
2023-05-18 14:00:28 +0700

    feat: add qrcode scan

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
-import { useState } from "react";
-
+import QrScanner from "qr-scanner";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [barCode, setBarCode] = useState("");
   const [link, setLink] = useState("");
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    const qrScanner = new QrScanner(videoRef.current!, (result) =>
+      {
+        if(result) {
+          setBarCode(result);
+        }
+      }
+    );
+    qrScanner.start();
+    () => {
+      qrScanner.stop();
+    };
+  }, []);
 
   const createLink = async () => {
     const payload = {
@@ -38,7 +53,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex items-center">
+      <video ref={videoRef}></video>
+      <div className="flex items-center mt-4">
         <p className="mr-4">Barcode uri</p>
         <input
           className="text-black p-1"
@@ -49,7 +65,9 @@ export default function Home() {
       <button className="mt-10 border p-2" onClick={createLink}>
         Create dynamic link
       </button>
-      <a className="mt-5 text-white" href={link}>{link}</a>
+      <a className="mt-5 text-white" href={link}>
+        {link}
+      </a>
       <div className="mb-auto" />
     </main>
   );
